d78c7825ee9723320e44cf575c3083d68b9a0114
Raushan <raushan.kumar.cer20@itbhu.ac.in>
2024-01-14 23:40:21 +0530

    first commit

---
+++ chai.js
@@ -0,0 +1,29 @@
+//select all buttons
+
+const buttons = document.querySelectorAll(".button");
+
+console.log(buttons);
+
+//select body
+
+const body = document.querySelector("body");
+console.log(body);
+
+buttons.forEach(function(button){
+    button.addEventListener("click", function(e){
+        console.log(e);
+        console.log(e.target);
+        if(e.target.id==="grey"){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id==="white"){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id==="yellow"){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id==="blue"){
+            body.style.backgroundColor = e.target.id;
+        }
+    })
+})
